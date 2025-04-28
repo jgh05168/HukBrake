719f185d05b4f05e406de1ff42b6d6881ba0fca0
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-04-28 23:54:52 +0900

    chore: Delete Debug Folder

--- HukBrake/DriveCoreMCU/Debug/src/common/core/qbuffer.d
+++
@@ -1,5 +0,0 @@
-src/common/core/qbuffer.o: ../src/common/core/qbuffer.c \
- ../src/common/core/qbuffer.h \
- C:/Embedded_Study/HukBrake/HukBrake/DriveCoreMCU/src/common/def.h
-../src/common/core/qbuffer.h:
-C:/Embedded_Study/HukBrake/HukBrake/DriveCoreMCU/src/common/def.h:
